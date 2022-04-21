ec097e0d1e5a90b72e9f834599a5ec78b1fbfed0
Flo-Gaupp <101625129+Flo-Gaupp@users.noreply.github.com>
2022-04-21 10:52:47 +0200

    Verbindung zwischen Punkten im Graph

--- src/lanchesterSimulation/LanchesterAnimation.java
+++ src/lanchesterSimulation/LanchesterAnimation.java
@@ -48,10 +48,15 @@ class LanchesterAnimationPanel extends JPanel {
 	public Dimension getPreferredSize() {
 		return new Dimension(_0_Constants.WINDOW_WIDTH, _0_Constants.WINDOW_HEIGHT);
 	}
+	
+	
+	
+	//START OF APPLICATION
 
 	int width = _0_Constants.WINDOW_WIDTH;
 	int height = _0_Constants.WINDOW_HEIGHT;
-	Populations test = new Populations (100000,100000,0.08,0.06);
+	Populations test = new Populations (90000,100000,0.08,0.06);
+	//Populations test = new Populations (100000,100000,0.08000001,0.08);
 	int diameter = 30;
 	int startGx = 100;
 	int startGy = 200;
@@ -157,7 +162,7 @@ class LanchesterAnimationPanel extends JPanel {
 			int xLength = endX-ursprungX;
 			int yLength = ursprungY-endY;
 			
-			//Koordinatensystem
+			//Koordinatensystem Basis Struktur
 			g.setColor(Color.black);
 			g.drawLine(ursprungX, ursprungY, ursprungX, endY);
 			g.drawLine(ursprungX, ursprungY, endX, ursprungY);
@@ -209,81 +214,26 @@ class LanchesterAnimationPanel extends JPanel {
 				graphGy [i] = (int) Math.floor(test.gStatus(i*scaleTimeDrawing));
 				graphHy [i] = (int) Math.floor(test.hStatus(i*scaleTimeDrawing));
 				
+				
+				//Draw Points
 				if (i<=time/scaleTimeDrawing) {
 					g.setColor(Color.RED);
 					g.fillOval(ursprungX+i-1,(int) (ursprungY-Math.round(graphGy[i]/scalePopulationDrawing)-1), 3, 3);
 					g.setColor(Color.BLUE);
 					g.fillOval(ursprungX+i-1,(int) (ursprungY-Math.round(graphHy[i]/scalePopulationDrawing)-1), 3, 3);
+					
+					
+					//Connect Points with Line
+					if (i!=0) {
+						g.setColor(Color.red);
+						g.drawLine(ursprungX+i, (int) (ursprungY-Math.round(graphGy[i]/scalePopulationDrawing)), ursprungX+i-1, (int) (ursprungY-Math.round(graphGy[i-1]/scalePopulationDrawing)));
+						g.setColor(Color.BLUE);
+						g.drawLine(ursprungX+i, (int) (ursprungY-Math.round(graphHy[i]/scalePopulationDrawing)), ursprungX+i-1, (int) (ursprungY-Math.round(graphHy[i-1]/scalePopulationDrawing)));
+					}
 				}
 			}
-			
-			
-			
-			
-			
-			
-			
-			
 			
 		}
 
 
-		/**
-		 * Exercises:
-		 * 
-		 * 1) Use the same initial conditions ( startX, startY, vX, vY ) as above. Let
-		 * the circle/ball "bounce off the vertical walls", i.e. provide for correct
-		 * changes of velocity whenever the circle/ball "collides elastically with" the
-		 * right-hand or the left-hand frame borders.
-		 * 
-		 * Hints: (i) As soon as the condition if( currentX >= width) yields "TRUE", do
-		 * the following - reverse vX: vX = -vX; - assign the value "width" to the
-		 * variable "startX"; - assign the value "time" to a new variable
-		 * "collisionTime", that you have to add to the code. (ii) Replace the code line
-		 * "currentX = startX + time * vX;" by "currentX = startX + (time -
-		 * collisionTime) * vX;"
-		 * 
-		 * Improve the application by ensuring that the ball does not penetrate into the
-		 * right-hand wall.
-		 * 
-		 * 2) Choose any initial conditions (startX, startY, vX, vY ). Let the
-		 * circle/ball "bounce off the walls", i.e. provide for correct changes of
-		 * velocity whenever the circle/ball "collides elastically with" any of the four
-		 * frame borders.
-		 * 
-		 * (i) As an alternative to the techniques employed in Exercise 1), use now what
-		 * we may call the "deltaTime paradigm":
-		 * 
-		 * double deltaTime = 0.0; double lastFrameTime = 0.0;
-		 * 
-		 * deltaTime = time - lastFrameTime; lastFrameTime = time; currentX = currentX +
-		 * (vX * deltaTime); currentY = currentY + (vY * deltaTime);
-		 * 
-		 * if (currentX >= _0_Constants.WINDOW_WIDTH - diameter) {
-		 * System.out.println("Object has hit the right-hand wall."); vX = -vX; currentX
-		 * = currentX - 1; // One pixel is subtracted from to the current x-coordinate
-		 * if the ball is at // the right-hand boundary. This prevents the ball from
-		 * "sticking to the border" after a collision
-		 * 
-		 * (ii) Note the following ways of formatting numerical output:
-		 *
-		 * System.out.println("vX = " + (double) Math.round(100 * vX) / 100);
-		 * System.out.println("currentX = " + (double) Math.round(100 * currentX) /
-		 * 100); System.out.println("currentY = " + (double) Math.round(100 * currentY)
-		 * / 100 + '\n');
-		 * 
-		 * 
-		 * 3) Simulate the motion of the ball/circle under the influence of gravity
-		 * Place the circle a some height h above the floor (bottom frame border) with
-		 * initial velocity vY = 0. Let the circle/ball undergo accelerated motion
-		 * toward the bottom. Once the ball hits the floor, its velocity is reversed
-		 * (fully elastic collision), the ensuing upward motion is decelerated until the
-		 * circle/ball comes to rest a height h, etc. * 4) As in Exercise 3) except that
-		 * there shall now be a loss of kinetic energy each time the ball hits the
-		 * botton.
-		 * 
-		 * 
-		 */
-
-	
-}
+	}
